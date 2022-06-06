a799ee06fa5e3a8e169f895f07c80777cb070762
chiarac20 <chiaracastelli89@gmail.com>
2022-06-06 17:48:08 +0100

    get random array logic added

--- 02-exercises/13-getRandomValues.js
+++ 02-exercises/13-getRandomValues.js
@@ -3,7 +3,7 @@
 // The function must return a random element of the array
 
 function getRandomElement (array) {
-    if (!array.length) throw new Error('No empty array');
+    // if (!array.length) throw new Error('No empty array');
     const randomIndex = parseInt(Math.random() * array.length);
     return array[randomIndex];
 }
@@ -16,5 +16,21 @@ function getRandomElement (array) {
 // The function must return an array with n element that will be randomly selected in ar without repetitions
 
 function getRandomArray (array, number) {
-
+    array.sort(() => Math.random() - 0.5)
+    return array.slice(0, number);
 }
+
+// console.log(getRandomArray([1,3,6,8,9, 3], 3))
+
+// Given an array of numbers find out if there is any reoccurrence
+
+const [,,...input] = process.argv;
+const numInput = input.map(n => parseInt(n));
+console.log(findReoccurrence(numInput));
+
+function findReoccurrence(numArray) {
+    numArray.sort();
+    return numArray.some((n, i) => n===numArray[i+1])
+}
+
+
